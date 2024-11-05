310d52f0b0302a32dcc50cff57008404a9da4999
Marouan el Marnissy <elmarnissymarouan@gmail.com>
2024-11-05 16:00:41 +0100

    Initial commit

---
+++ resources/views/layouts/frontend.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title' - 'Marouan Portfolio')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p class="text-center">Copyright &copy; 2019 Marouan Portfolio</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+</html>
