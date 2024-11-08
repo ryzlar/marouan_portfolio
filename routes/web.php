d566c9f115ae3f562762f9a68407698874052b99
Marouan el Marnissy <elmarnissymarouan@gmail.com>
2024-11-08 11:29:06 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,13 +5,16 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 Route::post('/contact', [ContactController::class, 'store'])->name('contact.store');
 Route::get('/contact', [ContactController::class, 'create'])->name('contact.create');
 Route::post('/projects/store', [ProjectController::class, 'store'])->name('projects.store');
 Route::get('/projects/create', [ProjectController::class, 'create'])->name('project.create');
 Route::get('/projects', [ProjectController::class, 'index'])->name('project.index');
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
+Route::delete('/dashboard', [ProjectController::class, 'destroy'])->name('contact.destroy');
 
 
 
